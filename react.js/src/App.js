92461897e3f9b06b54d01645177d31425c632271
bilal ahmad <bilal2003170@gmail.com>
2023-10-23 00:48:40 -0700

    all set

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <>
-    <NavBar title="TextUtils" aboutText="about us"/>
+    <NavBar title="Text" aboutText="about"/>
     </>
   );
 }
